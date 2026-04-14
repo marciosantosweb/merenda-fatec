1dec2df3b1f9e3db9348e6b4d9ba53f9c54e3e05
Marcio Santos <marcio@marciosantos.com.br>
2026-04-13 23:12:00 -0300

    Admin: Seguranca: Remove rotina agressiva do git clean do script deploy

--- deploy.php
+++ deploy.php
@@ -14,7 +14,6 @@ $output = shell_exec('git pull origin main 2>&1');
 if (strpos($output, 'unknown index entry format') !== false || strpos($output, 'index file corrupt') !== false) {
     echo "<b>Aviso: Índice do Git local corrompido. Iniciando Auto-Reparo...</b>\n\n";
     shell_exec('rm -f .git/index');
-    shell_exec('git clean -fd');
     shell_exec('git reset --hard origin/main');
     $output = shell_exec('git pull origin main 2>&1');
     echo "Reparo efetuado! Novo log do git pull:\n";
